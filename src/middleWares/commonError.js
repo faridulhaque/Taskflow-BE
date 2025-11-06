6e8f61357e1ae5b47c25ec82490543ccc59eab8b
= <=>
2025-11-06 13:22:18 +0600

    jwt eerror fixed

--- src/middleWares/commonError.js
+++ src/middleWares/commonError.js
@@ -1,7 +1,7 @@
+const commonError = (error) => {
+  console.log("error:", error);
+  res.status(500).json({ message: "something went wrong" });
+};
 
-const commonError = (error) =>{
-    console.log(error)
-    res.status(500).json({message: "something went wrong"})
-}
 
-module.exports = {commonError}
+module.exports = { commonError };
